b5526b7edf08518b959820223d4733a06facb9a2
Сергей <@DESKTOP-5JQRDG9>
2021-08-06 12:51:44 +0300

    les3

---
+++ Les3Kataev/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Les3Kataev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int num = 0;
+
+            do
+            {
+                num = int.Parse(Console.ReadLine());
+                if (num > 0 && num % 2 == 0)
+                    sum += num;
+
+            } while (num != 0);
+
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
