7a60e1d3e17ddf37c5256c503c6341ac3d182974
Сергей <@DESKTOP-5JQRDG9>
2021-08-06 12:42:02 +0300

    les2

--- Les2Kataev/Program.cs
+++ Les2Kataev/Program.cs
@@ -6,7 +6,17 @@ namespace Les2Kataev
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите число:");
+            int num = int.Parse(Console.ReadLine());
+            int i = 0;
+            while (num > 0)
+            {
+                i++;
+                num /= 10;
+            }
+                Console.WriteLine("Количество цифр =" + i);
+                return;
+            
         }
     }
 }
