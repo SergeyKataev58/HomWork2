2dc601dc18c5714365b037fcbc3cc05830502b96
Сергей <@DESKTOP-5JQRDG9>
2021-08-06 12:33:03 +0300

    Добавьте файлы проекта.

---
+++ HomWork2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число:");
+            Console.Write("Введите второе число:");
+            Console.Write("Введите третье число:");
+        }
+    }
+}
---
+++ Les1Kataev/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Les1Kataev   // Задача минимальное из трёх чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число а:");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите число b:");
+
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите число c:");
+
+            int c = int.Parse(Console.ReadLine());
+
+            if (a < b)
+                if (a < c)
+
+                Console.WriteLine("Наименьшее число  a=" + a);
+
+               else
+                
+               Console.WriteLine("Наименьшее число c=" + c);
+                
+            else if (b < c)
+                
+                Console.WriteLine("Наименьшее число b=" + b);
+                else
+                Console.WriteLine("Наименьшее число c=" + c);
+        }
+    }
+}
